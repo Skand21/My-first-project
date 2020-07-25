c9b85a88e23474cb8ea04ac2a1b34c78886acd60
Skand1 <68154802+Skand21@users.noreply.github.com>
2020-07-25 17:56:56 +0300

    Недоделал

--- Check.py
+++ Check.py
@@ -1,5 +1,12 @@
-try:
-    print ('Hello, bro!!!')
-except:
-    print ('Поймано исключение')
-print ('Программа закончилась')
+p = 600851475143
+
+for q in range (2, p):
+  if (p % q == 0):
+    z = p / q
+    k = 2
+    while ((z % k) != 0) and ((z != k )):
+      z = z - 1
+      for k in range (2,z):
+        conclusion = (k)
+    if conclusion == 0:
+      print (conclusion)
--- Euler project.py
+++ Euler project.py
@@ -1,9 +1,22 @@
-'''Простое число'''
+'''Простое число №3'''
 
-number = int(input ('Enter a number '))
+number = int(46)
+
+for p in range (2, number + 1):
+  while ((number % p) != 0) and (number != p):
+    print (p)
+
+
+
+
+
+
+
 delit = 2
+conclusion = 0
 while ((number % delit) != 0) and ((number != delit)):
-  for delit in range (1,number):
-    print (delit)
-
-print ('The programme is finished')
+  number = number - 1
+  for delit in range (2,number + 1):
+    conclusion = (delit)
+if conclusion == 0:
+  print (conclusion)
