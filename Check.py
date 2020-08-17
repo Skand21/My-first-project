921ee657c8a61855d5f2894b042baa36b8ed00e8
Skand1 <68154802+Skand21@users.noreply.github.com>
2020-08-17 17:46:04 +0300

    New

--- Check.py
+++ Check.py
@@ -1,21 +1,20 @@
-#Simple Numbers
+def bread(func):
+    def wrapper():
+        print()
+        func()
+        print("<\______/>")
+    return wrapper
 
-numbermain = 600851475143
+def ingredients(func):
+    def wrapper():
+        print("помидоры")
+        func()
+        print("салат")
+    return wrapper
 
-x = 0
-z = 0
+@bread
+@ingredients
+def sandwich(food="--ветчина--"):
+    print(food)
 
-for f in range (2, numbermain):
-  if (numbermain % f == 0):
-    numbermax = numbermain / f
-    for i in range (2, int(numbermax + 1)):
-      if i == numbermax:
-        x = 1
-      while x == 0:
-        if ((numbermax % i) == 0):
-          x = 1
-          z += 1
-        else:
-          x = 1
-    if z == 0:
-      print (numbermax)
+sandwich()
